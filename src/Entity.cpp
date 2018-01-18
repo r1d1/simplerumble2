33eff5bdc2f8361676642a37838209a9aeba3797
Erwan <r1d1@github.com>
2018-01-18 19:24:44 +0100

    Adding and using Entity for better management of resources

---
+++ src/Entity.cpp
@@ -0,0 +1,49 @@
+#include "Entity.h"
+
+//Entity::Entity(std::string entity_name, std::string texture_name)
+Entity::Entity(std::string entity_name, sf::Texture * ptexture = NULL)
+{
+    pos_x = 0.;
+    pos_y = 0.;
+    graphicsUpdated=false;
+    dataUpdated=false;
+    banaana = 0.;
+    count = -1;
+    // Should go in an animation class:
+    animFrame = 0;
+    cumulativeTime = 0.;
+    texture = ptexture;
+    sprite.setTexture(*ptexture);
+	// to be done intelligently:
+    //sprite.setScale(0.2f, 0.2f);
+    sprite.setPosition(pos_x, pos_y);
+}
+
+Entity::~Entity(){}
+
+bool Entity::getGraphicsFlag(){ return graphicsUpdated; }
+
+bool Entity::getDataFlag(){ return dataUpdated; }
+
+void Entity::move(float dx=0., float dy=0.)
+{
+    pos_x += dx;
+    pos_y += dy;
+    sprite.setPosition(pos_x, pos_y);
+}
+
+void Entity::animate(float elapsedTime)
+{
+    if(cumulativeTime > 0.25)
+    {
+        animFrame = (animFrame + 1) % 4;
+        cumulativeTime = 0.0;
+    }
+    else{ cumulativeTime += elapsedTime; }
+}
+
+void Entity::render(sf::RenderWindow * app)
+{
+    sprite.setTextureRect(sf::IntRect(animFrame*32, 48, 32, 48));
+    app->draw(sprite);
+}
--- src/Entity.h
+++ src/Entity.h
@@ -8,10 +8,42 @@
  *
  * */
 
+#include <string>
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
 class Entity
 {
     private:
+        std::string name;
+        float pos_x;
+        float pos_y;
+
+        int count;
+
+        bool graphicsUpdated;
+        bool dataUpdated;
+
+        int animFrame;
+        float cumulativeTime;
+        float banaana;
+        sf::Sprite sprite;
+        // use pointer for texture to avoid loading the texture for each entity
+        sf::Texture * texture;
+
     public:
+        //Entity();
+        Entity(std::string entity_name, sf::Texture * texture_name);
+        ~Entity();
+
+        bool getGraphicsFlag();
+        bool getDataFlag();
+        // update entity location
+        //void move();
+        void move(float dx, float dy);
+        // update graphical part
+        void animate(float elapsedTime);
+        void render(sf::RenderWindow * app);
 };
 
 #endif
