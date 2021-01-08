27bafd490ef90cc17c9d0948b16756a096ec6f52
r1d1 <r1d1@users.noreply.github.com>
2021-01-08 18:09:20 +0100

    Fixing debug text format

--- src/Agent.cpp
+++ src/Agent.cpp
@@ -38,7 +38,7 @@ int Agent::getShield() const { return m_Shield; }
 bool Agent::isAlive() const { return m_Life > 0; }
 void Agent::displayState() const
 {
-	std::cout << "I'm \033[1m" << m_Name << "\033[21m, I've \033[" << ((float(m_Life) / float(m_MaxLife) < 0.3) ? "91m" : ((float(m_Life) / float(m_MaxLife) < 0.6) ? "93m" : "92m")) << m_Life << "\033[39m/" << m_MaxLife << " HP and I'm fighting with \033[1m" << ( (m_AbilityOne != 0) ? m_AbilityOne->getType() + " " + m_AbilityOne->getName() : "nothing") << "\033[21m that does " << ( (m_AbilityOne != 0) ?  m_AbilityOne->getDamage() : 0) << std::endl;
+	std::cout << "I am \033[32m" << m_Name << "\033[0m, I've \033[" << ((float(m_Life) / float(m_MaxLife) < 0.3) ? "91m" : ((float(m_Life) / float(m_MaxLife) < 0.6) ? "93m" : "92m")) << m_Life << "\033[39m/" << m_MaxLife << " HP and I'm fighting with \033[34m" << ( (m_AbilityOne != 0) ? m_AbilityOne->getType() + " " + m_AbilityOne->getName() : "nothing") << "\033[0m that does " << ( (m_AbilityOne != 0) ?  m_AbilityOne->getDamage() : 0) << std::endl;
 }
 void Agent::resetCharacter()
 { 
